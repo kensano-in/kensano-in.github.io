f4100e5512e62bbd3d56f7255b09774717425ac4
kensano-in <shinichiro.in@gmail.com>
2026-02-05 02:16:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-const canvas = document.getElementById("void");
-const ctx = canvas.getContext("2d");
-
-function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-resize();
-window.addEventListener("resize", resize);
-
-let stars = Array.from({ length: 80 }, () => ({
-  x: Math.random() * canvas.width,
-  y: Math.random() * canvas.height,
-  r: Math.random() * 1.2 + 0.3,
-  a: Math.random() * 0.5 + 0.1
-}));
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  stars.forEach(s => {
-    ctx.fillStyle = `rgba(255,255,255,${s.a})`;
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-    ctx.fill();
-    s.y += 0.05;
-    if (s.y > canvas.height) s.y = 0;
-  });
-  requestAnimationFrame(draw);
-}
-
-draw();
+const bio=document.getElementById('bio');
+function show(w){bio.innerHTML=w==='shin'?'<strong>Founder</strong><br>Authorship, narrative direction.<br><em>Building meaning.</em>':'<strong>Co-founder</strong><br>Systems & execution.<br><em>Turning vision into permanence.</em>';bio.classList.add('active')}
+function openIG(){location.href='https://instagram.com/verlyn.in'}
+function openShinken(){location.href='https://shinken.in'}
